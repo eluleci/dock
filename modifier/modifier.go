d51f8b144f38f45994b65dee0d7eb16ca64d729f
Emrullah Luleci <emrullahluleci@gmail.com>
2015-12-10 23:24:00 +0200

    fix bug about expand with aggregation

--- modifier/modifier.go
+++ modifier/modifier.go
@@ -1,20 +1,23 @@
 package modifier
+
 import (
 	"strings"
 	"fmt"
 	"github.com/eluleci/dock/adapters"
 	"github.com/eluleci/dock/messages"
 	"github.com/eluleci/dock/utils"
+	"reflect"
+	"gopkg.in/mgo.v2/bson"
 )
 
-var ExpandArray = func(data map[string]interface {}, config string) (result map[string]interface{}, err *utils.Error) {
+var ExpandArray = func(data map[string]interface{}, config string) (result map[string]interface{}, err *utils.Error) {
 
-	dataArray := data["data"].([]map[string]interface {})
+	dataArray := data["data"].([]map[string]interface{})
 
 	resultArray := make([]map[string]interface{}, len(dataArray))
 	for i, v := range dataArray {
 		var expandedObject map[string]interface{}
-		expandedObject, err = ExpandItem(map[string]interface {}(v), config)
+		expandedObject, err = ExpandItem(map[string]interface{}(v), config)
 		if err != nil {
 			return
 		}
@@ -40,22 +43,26 @@ func ExpandItem(data map[string]interface{}, config string) (result map[string]i
 		if containsChildToExpand {
 			trimmedField = field[0:strings.Index(field, "(")]
 		}
-		fmt.Println("self: " + trimmedField)
+		fmt.Println("child: " + trimmedField)
 
-		// expanding direct children first
 		reference := data[trimmedField]
 		if reference == nil {
 			continue
 		}
+
 		var expandedObject map[string]interface{}
-		expandedObject, err = fetchData(reference.(map[string]interface{}))
-		if err != nil {
-			return
+		if isValidReference(reference.(map[string]interface{})) {
+			expandedObject, err = fetchData(reference.(map[string]interface{}))
+			if err != nil {
+				return
+			}
+		} else {
+			expandedObject = reference.(map[string]interface{})
 		}
 
 		// expanding children
 		if containsChildToExpand {
-			expandConfigOfChild := field[strings.Index(field, "(")+1:strings.LastIndex(field,")")]
+			expandConfigOfChild := field[strings.Index(field, "(") + 1:strings.LastIndex(field, ")")]
 			fmt.Println("childToExpand: ")
 			fmt.Println(expandedObject)
 			fmt.Println("expandConfigOfChild: " + expandConfigOfChild)
@@ -66,7 +73,7 @@ func ExpandItem(data map[string]interface{}, config string) (result map[string]i
 				return
 			}
 			fmt.Println(expandedChild)
-//			expandedObject[trimmedField] = expandedChild
+			//			expandedObject[trimmedField] = expandedChild
 		}
 
 		data[trimmedField] = expandedObject
@@ -79,14 +86,22 @@ func ExpandItem(data map[string]interface{}, config string) (result map[string]i
 var fetchData = func(data map[string]interface{}) (object map[string]interface{}, err *utils.Error) {
 	fmt.Println("fetchData: ");
 	fmt.Println(data);
+	fmt.Println(reflect.TypeOf(data["_id"]))
 
-	id := data["_id"].(string)
+	fieldType := reflect.TypeOf(data["_id"])
+
+	var id string
+	if fieldType.Kind() == reflect.String {
+		id = data["_id"].(string)
+	} else {
+		id = data["_id"].(bson.ObjectId).Hex()
+	}
 	className := data["_class"].(string)
 	dbAdapter := &adapters.MongoAdapter{adapters.MongoDB.C(className)}
 
 	var rw messages.RequestWrapper
 	var m messages.Message
-	m.Res = "/"+className+"/" + id
+	m.Res = "/" + className + "/" + id
 	rw.Message = m
 
 	object, err = adapters.HandleGetById(dbAdapter, rw)
@@ -98,4 +113,11 @@ var fetchData = func(data map[string]interface{}) (object map[string]interface{}
 	fmt.Println(object)
 	fmt.Println("--------")
 	return
+}
+
+var isValidReference = func(data map[string]interface{}) (bool) {
+	t, hasType := data["_type"]
+	_, hasId := data["_id"]
+	_, hasClass := data["_id"]
+	return len(data) == 3 && hasType && hasId && hasClass && t == "reference"
 }
