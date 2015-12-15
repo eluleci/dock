a5a3f1e9b2b445932d5c4d4b956c9ad64e420a61
Emrullah Luleci <emrullahluleci@gmail.com>
2015-12-15 13:12:39 +0200

    save _id as string instead of object id

--- adapters/mongoadapter.go
+++ adapters/mongoadapter.go
@@ -50,7 +50,7 @@ var HandleGetById = func(m *MongoAdapter, requestWrapper messages.RequestWrapper
 	id := message.Res[strings.LastIndex(message.Res, "/") + 1:]
 	response = make(map[string]interface{})
 
-	getErr := m.Collection.FindId(bson.ObjectIdHex(id)).One(&response)
+	getErr := m.Collection.FindId(id).One(&response)
 	if getErr != nil {
 		err = &utils.Error{http.StatusNotFound, "Item not found."};
 		response = nil
@@ -127,7 +127,7 @@ var HandlePut = func(m *MongoAdapter, requestWrapper messages.RequestWrapper) (r
 	id := message.Res[strings.LastIndex(message.Res, "/") + 1:]
 
 	objectToUpdate := make(map[string]interface{})
-	findErr := m.Collection.FindId(bson.ObjectIdHex(id)).One(&objectToUpdate)
+	findErr := m.Collection.FindId(id).One(&objectToUpdate)
 	if findErr != nil {
 		err = &utils.Error{http.StatusNotFound, "Item not found."};
 		return
@@ -138,7 +138,7 @@ var HandlePut = func(m *MongoAdapter, requestWrapper messages.RequestWrapper) (r
 		objectToUpdate[k] = v
 	}
 
-	updateErr := m.Collection.UpdateId(bson.ObjectIdHex(id), objectToUpdate)
+	updateErr := m.Collection.UpdateId(id, objectToUpdate)
 	if updateErr != nil {
 		err = &utils.Error{http.StatusInternalServerError, "Update request to db failed."};
 		return
@@ -155,7 +155,7 @@ var HandleDelete = func(m *MongoAdapter, requestWrapper messages.RequestWrapper)
 	message := requestWrapper.Message
 	id := message.Res[strings.LastIndex(message.Res, "/") + 1:]
 
-	removeErr := m.Collection.RemoveId(bson.ObjectIdHex(id))
+	removeErr := m.Collection.RemoveId(id)
 	if removeErr != nil {
 		err = &utils.Error{http.StatusNotFound, "Item not found."};
 		return
